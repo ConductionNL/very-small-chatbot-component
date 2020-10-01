d126f63b464340a6ba2ceb4e8fe8a8d5666a2166
Ruben van der Linde <ruben@conduction.nl>
2020-10-01 09:12:51 +0200

    Finishing first draft

--- api/src/Service/MessageService.php
+++ api/src/Service/MessageService.php
@@ -23,12 +23,14 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class MessageService
 {
+    private $newrequest;
     private $em;
     private $commongroundService;
     private $ptcService;
 
     public function __construct(EntityManagerInterface $em, CommonGroundService $commongroundService, PtcService $ptcService)
     {
+        $this->newrequest = false;
         $this->em = $em;
         $this->commongroundService = $commongroundService;
         $this->ptcService = $ptcService;
@@ -36,8 +38,20 @@ class MessageService
 
     public function getResponce(Message $message, string $proccesId)
     {
+        $response = [];
+
         // vraag 1 is er een converstion?
         $conversation = $this->getConversation($message, $proccesId);
+
+        // Lets esteblish if there is a new request
+        if($this->newrequest){
+            $response[] = [
+                'text'=>'Bedankt voor uw verhuis verzoek, om uw aanvraag te kunnen verwerken hebben we een verzoek voor u aangemaakt onder nummer '.$this->newrequest.'. U kunt uw verzoek op ieder moment inzien en wijzigen via onze webstie',
+                'buttons'=> [
+                    ["title"=>"Verzoek inzien","payload"=>"https://zuid-drecht.nl/"]
+                ]
+            ];
+        }
 
         // Kijken of er een vraag wordt benatwoord
         if($property = $conversation->getLastQuestion()){
@@ -47,25 +61,35 @@ class MessageService
 
             // Get the request
             $request = $conversation->getRequest();
+            $request = $this->commongroundService->getResource($request);
 
             // Write our message to the request
             $request['properties'][$property['name']] = $message->getMessage();
 
+            /* @todo nlu integratie */
+
             // Save our data
-            $this->commongroundService->save($request);
+            $this->commongroundService->saveResource($request);
+            $response[] = ['text'=> 'Uw gekozen '.$property['title'].' is '. $request['properties'][$property['name']]];
         }
 
+        // set the lats question
         $conversation->setLastQuestion($this->getNextQuestion($conversation));
+        // And save it to the database
+        $this->em->persist($conversation);
+        $this->em->flush();
 
         $property =$this->commongroundService->getResource($conversation->getLastQuestion());
 
         // responce genereren
         if(array_key_exists('utter', $property)){
-            return $property['utter'];
+            $response[] = ['text'=> $property['utter']];
         }
         else{
-            return $property['title'];
+            $response[] = ['text'=> $property['title']];
         }
+
+        return $response;
 
     }
 
@@ -75,7 +99,7 @@ class MessageService
         $conversation = $this->em->getRepository('App\Entity\Conversation')
         ->findOneBy(['sender'=>$message->getSender(),'proccess'=>$proccesId]);
 
-        if($conversation['']){
+        if($conversation){
             return $conversation;
         }
         else{
@@ -88,14 +112,22 @@ class MessageService
         $request['organization'] = $this->commongroundService->cleanUrl(['component'=>'wrc','type'=>'organizations','id'=> '4d1eded3-fbdf-438f-9536-8747dd8ab591' ]);
         $request['properties'] = [];
 
-        // Verzoek opslaan
+        // Lets create the request
         $request = $this->commongroundService->saveResource($request,['component'=>'vrc','type'=>'requests']);
 
+        // Lets allert that there is a new request
+        $this->newrequest = $request['reference'];
+
+        // Lets create the converstation
         $conversation = New Conversation();
         $conversation->setProccess($procces['id']);
         $conversation->setRequest($request['@id']);
         $conversation->setSender($message->getSender());
         $conversation->getLastQuestion(null);
+
+        // And save it to the database
+        $this->em->persist($conversation);
+        $this->em->flush();
 
         return $conversation;
         }
@@ -110,12 +142,10 @@ class MessageService
         $proccess = $this->commongroundService->getResource($proccess);
 
         // last question moet altijd een vtc property zijn
-        $procces = $this->ptcService->extendProcess($proccess, $request);
+        $proccess = $this->ptcService->extendProcess($proccess, $request);
 
         foreach ($proccess['stages'] as $stage){
             foreach ($stage['sections'] as $section){
-                var_dump($section);
-                die;
                 foreach ($section['propertiesForms'] as $property){
                     // Returnen op de éerste niet valid vraag
                     if(!$property['valid']){
